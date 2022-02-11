b0eb4aec9a3d6de96670ab2ee83f0b9605ae4ebd
Aintzane Morales <aintzanemorales1992@gmail.com>
2022-02-11 11:29:50 +0100

    created components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
 import './App.css';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+const title = "Expense tracker"
 
 function App() {
   return (
-    <div className="App">
-      app page
+    <div className="">
+      <Header title= {title}/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransaction/>
+      </div>
     </div>
   );
 }
